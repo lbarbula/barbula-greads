755c919d63550a2ecc58b9772be91b48db6f634c
Lucas Barbula <lucas.barbula@gmail.com>
2016-07-12 11:22:38 -0600

    migrated in production

--- knexfile.js
+++ knexfile.js
@@ -4,5 +4,9 @@ module.exports = {
   development:  {
     client: 'pg',
     connection: 'postgres://localhost/galvanize-reads'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABSE_URL + '?ssl=true'
   }
 }
