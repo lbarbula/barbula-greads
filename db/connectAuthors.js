633ae80ac0b83207db6a389d536913b64c0b1098
Lucas Barbula <lucas.barbula@gmail.com>
2016-07-12 15:06:09 -0600

    working add author

--- db/connectAuthors.js
+++ db/connectAuthors.js
@@ -3,5 +3,8 @@ var knex = require('./knex')
 module.exports = {
   listAuthors: () => {
     return knex('author')
-  }
+  },
+  addAuthor: (body) => {
+    return knex('author').insert(body)
+  },
 }
--- routes/authors.js
+++ routes/authors.js
@@ -9,4 +9,13 @@ router.get('/', function(req, res, next){
     })
 })
 
+router.get('/new', function(req, res, next){
+  res.render('authors/add-authors')
+})
+router.post('/new', function(req, res, next){
+  db.addAuthor(req.body)
+  .then(function(){
+    res.redirect('/authors')
+  })
+})
 module.exports = router;
