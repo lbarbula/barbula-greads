3f7a5d8ba55e2812de1d4c7bed6a50ceb003ae41
Lucas Barbula <lucas.barbula@gmail.com>
2016-07-12 12:26:09 -0600

    clean form

--- db/api.js
+++ db/api.js
@@ -3,6 +3,8 @@ var knex = require('./knex')
 module.exports = {
   listBooks: () => {
     return knex('book')
+      .join('genre', 'genre.id', 'book.genre_id')
+        .select('book_name as bookName', 'book.book_description', 'book.cover_url', 'genre.name as genreName')
   },
   getGenre: () => {
     return knex('genre')
